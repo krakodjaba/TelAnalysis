cba758926dea2e53db0865e2b38efa79aafe170b
Eduard Isaev <107117398+krakodjaba@users.noreply.github.com>
2024-10-14 12:29:02 -0400

    Улучшение
    - Исправлены ошибки и улучшена обработка результатов анализа.
    - Оптимизирован код для повышения читаемости и удобства использования.
    - Попытка Анализа эмоций

--- channel_analyse.py
+++ channel_analyse.py
@@ -1,76 +1,63 @@
-import json, jmespath, nltk_analyse,utils, time
+import json
+import jmespath
+import nltk_analyse
+import utils
+import time
 from pywebio import input, config
-from pywebio.output import put_html,put_text,put_image, put_button, put_code, clear, put_file, put_table
-from pywebio.input import file_upload as file
-from pywebio.session import run_js
+from pywebio.output import put_html, put_text, put_image, put_table
 from wordcloud import WordCloud
-import matplotlib.pyplot as plt
-from utils import read_conf
 
-global select_type_stem
-select_type_stem = read_conf('select_type_stem')
 
+# Чтение конфигурации
+select_type_stem = utils.read_conf('select_type_stem')
+most_com = utils.read_conf('most_com_channel')
 
 def channel(filename):
-    most_com = read_conf('most_com_channel')
-    filename = filename.split(".")[0]
-    filename = filename.split("/")[1]
+    # Извлечение имени канала из файла
+    filename = filename.split(".")[0].split("/")[1]
     with open(f'asset/{filename}.json', 'r', encoding='utf-8') as f:
-        text_list = list()
-        #f = f.readlines()
         jsondata = json.load(f)
-        name_channel = jmespath.search('name',jsondata)
-        put_html(f"<center><h1>{name_channel}</h1><center>")
-        messages_find = jmespath.search('messages[*].text',jsondata)
-        #print(*messages_find, sep='\n\n-')
+        name_channel = jmespath.search('name', jsondata)
+
+        # Отображение имени канала
+        put_html(f"<center><h1>{name_channel}</h1></center>")
+        messages_find = jmespath.search('messages[*].text', jsondata)
+
+        text_list = []
+        
+        # Обработка сообщений канала
         for message in messages_find:
-            if str(type(message)) == "<class 'list'>":
-                #print(message)
-                len_message = len(message)
+            if isinstance(message, list):
                 for mes in message:
-                    # print(mes)
-                    try:
-                        text = jmespath.search('text',mes)
-                        if text is None:
-                            mes = utils.remove_emojis(mes)
-                            text_list.append(mes)
-                        else:
-                            text = utils.remove_emojis(text)
-                            text_list.append(text)
-                            
-                    except:
-                        mes = utils.remove_emojis(mes)
-                        text_list.append(mes)
-                        #print(mes)
+                    text = jmespath.search('text', mes) or mes
+                    text_list.append(utils.remove_emojis(text))
             else:
-                message = message.replace("   "," ").replace("\n","").replace("\t","").strip()
+                message = message.replace("   ", " ").replace("\n", "").replace("\t", "").strip()
                 if len(message) > 4:
-                    message = utils.remove_emojis(message)
-                    text_list.append(message)
-        #print(len(text_list))
+                    text_list.append(utils.remove_emojis(message))
+
+        # Анализ текста и генерация облака слов
         fdist, tokens = nltk_analyse.analyse(text_list, most_com)
-        all_tokens = list()
-        #print(tokens)
-        for token in tokens:
-            all_tokens.append(token)
-            #print(token)
-        #print(len(all_tokens))
+        all_tokens = list(tokens)
         all_tokens, data = nltk_analyse.analyse_all(all_tokens, most_com)
-        #print(all_tokens)
+        
+        # Генерация облака слов
         text_raw = " ".join(data)
-        #max_wordss = (10 / 100) * len(data)
         wordcloud = WordCloud().generate(text_raw)
         filename_path = f'asset/{filename}_wordcloud.png'
-        wordcloud = wordcloud.to_image()
-        wordcloud.save(filename_path)
-        img = open(filename_path,'rb').read()
+        wordcloud.to_file(filename_path)
+
+
+        # Отображение результата
+        with open(filename_path, 'rb') as img_file:
+            img = img_file.read()
+        
         time.sleep(2)
         put_text(f"Wordcloud[{most_com}]:")
         put_image(img, width='600px')
         put_text(f"\nCount of all tokens: {len(tokens)}")
         put_text(f"\nСhannel frequency analysis[{most_com}]:")
-        gemy = []
-        for x,y in all_tokens:
-            gemy.append([x,y])
-        all_tokens.clear()
+
+        # Форматирование данных для таблицы
+        gemy = [[x, y] for x, y in all_tokens]
         put_table(gemy, header=['word', 'count'])
--- words_analyze.py
+++ words_analyze.py
@@ -1,207 +1,227 @@
 from utils import remove_chars_from_text, remove_emojis, clear_user, read_conf
 import nltk_analyse
 import sys
-from pywebio import input, config
-from pywebio.output import put_html,put_text,put_image,put_collapse, put_button, put_code, clear, put_file, popup, put_table
-from pywebio.input import file_upload as file
-from pywebio.session import run_js
-import json, re, jmespath, string, collections
-from pywebio.input import input
-from pywebio.pin import *
+from pywebio import config, output, pin, session
+import json, re, jmespath
+from validate_email import validate_email
+import phonenumbers
+import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-config(theme='dark',title="TelAnalysis", description="Analysing Telegram CHATS-CHANNELS-GROUPS")
-put_button("Scroll Down",onclick=lambda: run_js('window.scrollTo(0, document.body.scrollHeight)'))
-put_button("Close",onclick=lambda: run_js('window.close()'), color='danger')
-put_html("<h1><center>Analyse of Telegram Chat<center></h1><br>")
-put_input('ID')
-put_button("Search ID",onclick=lambda: run_js(f'window.find({pin.ID}, true)'), color='warning')
-all_tokens = []
-users = []
+# Глобальные переменные
+emails, phoness, all_tokens, users = [], [], [], {}
 count_messages = 0
-filename = sys.argv[1]
-filename = filename.split(".")[0]
-filename = filename.split("/")[1]
+
+
+action_map = {
+    'invite_members': 'Invite Member',
+    'remove_members': 'Kicked Members',
+    'join_group_by_link': 'Joined by Link',
+    'pin_message': 'Pinned Message',
+    # Добавьте другие действия по необходимости
+}
+
+action_reverse   = ['Invite Member','Kicked Members','Joined by Link', 'Pinned Message']
+
+analyzer = SentimentIntensityAnalyzer()
+
+# Конфигурация интерфейса
+config(theme='dark', title="TelAnalysis", description="Analysing Telegram CHATS-CHANNELS-GROUPS")
+output.toast(content='Wait..', duration=2)
+output.put_button("Scroll Down", onclick=lambda: session.run_js('window.scrollTo(0, document.body.scrollHeight)'))
+output.put_button("Close", onclick=lambda: session.run_js('window.close()'), color='danger')
+output.put_html("<h1><center>Analyse of Telegram Chat<center></h1><br>")
+
+# Ввод ID пользователя
+pin.put_input('ID')
+output.put_button("Search ID", onclick=lambda: session.run_js(f'window.find({pin.pin.ID}, true)'), color='warning')
+
+# Открытие файла и загрузка данных
+filename = sys.argv[1].split(".")[0].split("/")[1]
+
 with open(f'asset/{filename}.json', 'r', encoding='utf-8') as datas:
     data = json.load(datas)
-    sf = jmespath.search('messages[*]',data)
-    group_name = jmespath.search('name', data)
-    for message in sf:
+
+sf = jmespath.search('messages[*]', data)
+group_name = jmespath.search('name', data)
+
+def analyze_sentiment(text):
+    try:
+        score = analyzer.polarity_scores(str(text))
+        return float(score['compound'])  # Приводим к float для уверенности
+    except:
+        return float(0.0)
+
+
+# Функция для извлечения email и телефонных номеров из текста
+def extract_emails_and_phone_numbers(text):
+    emails_list = []
+    emails = re.findall(r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}', text)
+    for email in emails:
+        if validate_email(email, verify=False):
+            emails_list.append(email)
+    phones_list = []    
+    phone_numbers = re.findall(r'\+?[0-9]{1,3}?[-. (]?[0-9]{1,4}[-. )]?[0-9]{1,4}[-. ]?[0-9]{1,9}', text)
+    for phones in phone_numbers:
         try:
-            user = jmespath.search('from_id', message)
-            if user and user != '':
-                #user = clear_user(user)
-                #print(user)
-                if user not in users and user is not None:
-                    #user = str(user).replace(" ","").replace('"','').replace(".","").replace("꧁","")
-                    try:
-                        user = user.replace(" ","")
-                    except:
-                        put_text("error #9")
-                    #print(user)
-                    users.append(user)
-                    
-                    try:
-                        exec('{}_list = []'.format(user))
-                    except Exception as ex:
-                        put_code(ex, user)
-                        continue
-        except Exception as ex:
-            print(ex, 92141)
-            continue
-    for message in sf:
-        count_messages +=1
-        texts = jmespath.search('text',message)
-        from_user = jmespath.search('from_id', message)
-        if from_user is None:
-            continue
-        else:
-            #from_user = clear_user(from_user)
-            from_user = from_user.replace(" ","")
-            #print(from_user)
-        if str(type(texts)) == "<class 'str'>":
-            if texts != '':
-                test = str(texts).replace("\\n","").replace("\n","").strip()
-                if "http" in test:
-                    continue
-                else:
-                    try:
-                        test = test.replace("\\n","").replace("\n","").replace('"',"'").strip()
-                        
-                        try:
-                            test = remove_emojis(test)
-                            
-                        except:
-                            test = test
-                        
-                    except:
-                        put_text("error #1")
-                    try:
-                        if test is None or test == "":
-                            continue
-                        else:
-                            exec('{}_list.append("{}")'.format(from_user,str(test)))
-                            #exec('id_{}_list = []'.format(user))
-                            #print('{}_list.append("{}")'.format(from_user,str(test)))
-                    except Exception as ex:
-                        print(ex)
-                        continue
-                    #count_messages +=1
-        elif str(type(texts)) == "<class 'list'>":
-            for textt in texts:
-                try:
-                    if len(textt['text']) >=1:
-                        test = textt['text']
-                        
-                        if "http" in test:
-                            continue
-                        else:
-                            try:
-                                test = test.replace("\\n","").replace("\n","").strip()
-                                try:
-                                    test = remove_emojis(test)
-                                except:
-                                    test = test
-                            except:
-                                put_text("error #2")
-                            try:
-                                if test is None or test == "":
-                                    continue
-                                else:
-                                    exec('{}_list.append("{}")'.format(from_user,str(test)))
-                                    #print('{}_list.append("{}")'.format(from_user,str(test)))
-                            except Exception as ex:
-                                continue
-                            #count_messages +=1
-                except Exception as ex:
-                    try:
-                        try:
-                            test = textt.replace("\\n","").replace("\n","").strip()
-                            try:
-                                test = remove_emojis(test)
-                            except:
-                                test = test
-                        except:
-                            put_text("error #3")
-                        try:
-                            if test is None or test == "":
-                                continue
-                            else:
-                                exec('{}_list.append("{}")'.format(from_user,str(test)))
-                                #print('{}_list.append("{}")'.format(from_user,str(test)))
-                        except Exception as ex:
-                            continue
-                        #count_messages +=1
-                    except:
-                        put_text("error #4")
+            phone_number = phonenumbers.parse(phones, None)
+            if phonenumbers.is_valid_number(phone_number):
+                phones_list.append(phones)
+        except Exception:
+            pass       
+    return emails_list, phones_list
+
+def extract_text_from_message(message):
+    texts = set()  # Используем множество, чтобы избежать дубликатов
     
+    if isinstance(message, dict):
+        text = jmespath.search('text', message)
+        if isinstance(text, str) and text.strip():
+            texts.add(text)
+
+        elif isinstance(text, list):
+            for item in text:
+                if isinstance(item, dict):
+                    item = jmespath.search('text', item)
+                    if item:
+                        texts.add(item)
+                elif isinstance(item, str):
+                    texts.add(item)
+
+        entities = jmespath.search('text_entities[*].text', message)
+        if entities:
+            for entity in entities:
+                texts.add(entity)
+
+        for value in message.values():
+            texts.update(extract_text_from_message(value))
+
+    elif isinstance(message, list):
+        for item in message:
+            texts.update(extract_text_from_message(item))
+
+    return texts
+
+# Функция для обработки каждого сообщения
+def process_message(message):
+    global count_messages
+    action = ""
+    user = jmespath.search('from_id', message)
+
+    if not user:
+        user = jmespath.search('actor_id', message)
+        if user:
+            user = user.replace(" ", "")
+            if user not in users:
+                users[user] = []
+
+            action = jmespath.search('action', message)
+            if action:
+                tex = jmespath.search('text', message) or ''
+                action_text = action_map.get(action, action)
+                
+                if action in ['invite_members', 'remove_members']:
+                    members = jmespath.search('members', message)
+                    members = ",".join(str(x) for x in members if x)
+                    users[user].append((f"{action_text} - {members}",0.0))
+                elif action == 'pin_message':
+                    users[user].append((f"{action_text} - {tex}",0.0))
+                else:
+                    users[user].append((f"{action_text} {tex}",0.0))
+                return
+
+    user = user.replace(" ", "")
+    if user not in users:
+        users[user] = []
+    count_messages += 1
+
+    unique_texts = extract_text_from_message(message)
+    for clean_text in unique_texts:
+        if clean_text:
+            #users[user].append(clean_text)
+            sentiment_score = analyze_sentiment(clean_text)
+            users[user].append((clean_text, sentiment_score))  # Сохраняем текст и балл
+            # Извлечение email и телефонных номеров
+            extracted_emails, extracted_phone_numbers = extract_emails_and_phone_numbers(clean_text)
+            emails.extend(extracted_emails)
+            phoness.extend(extracted_phone_numbers)
+
+# Обработка сообщений с использованием потоковой обработки
+with ThreadPoolExecutor() as executor:
+    executor.map(process_message, sf)
+
+# Теперь отображаем в нужном формате user - user_from
+for user, da in users.items():
+    #print(da)
+    user_from = ""
+    messages = jmespath.search(f"messages[*]", data)
+    
+    for m in messages:
+        if jmespath.search('from_id', m) == user:
+            user_from = jmespath.search('from', m)
+            break
+    
+    if user_from:
+        user_display = f"{user_from} - {user}"
+    else:
+        user_display = user
+        
+        
+    # Извлечение оценок чувствительности
+    user_sentiment_scores = []
+    for x in da:
+        try:
+            score = float(x[1])  # Приводим к float
+            user_sentiment_scores.append(score)
+        except (ValueError, TypeError):
+            # Игнорируем элементы, которые не могут быть преобразованы в float
+            #print(f"Invalid sentiment score: {x}")
+            continue
+    average_user_sentiment = sum(user_sentiment_scores) / len(user_sentiment_scores) if user_sentiment_scores else 0
     
     try:
-        put_table([count_messages, len(users)], header=['All used messages count','Messages from count users detected'])
+        most_com = read_conf('most_com')
+        genuy, tokens = nltk_analyse.analyse(da, most_com)
+
+        gemy = [[x, y] for x, y in genuy]
+        gery = [[x[0]] for x in da]
+        all_tokens.extend(tokens)
+
+        if gery or gemy:
+            output.put_collapse(user_display, [
+        f'Messages of {user_display}',  # Сообщение с user_from
+        output.put_text(f'Average Sentiment for {user_display}: {average_user_sentiment:.2f}'),
+        output.put_table([[x[0], x[1]] for x in da], header=['Messages', 'Sentiment Score']),
+        output.put_table(gemy, header=['word', 'count'])
+    ], open=False)
+
     except Exception as ex:
-        put_text(ex)
-    
-    for i,user in enumerate(users):
-        #print(users)
-        try:
-            try:
-                if user and user != "":
-                    user = user.replace(" ","")
-                    exec('da = {}_list'.format(user))
-                    most_com = read_conf('most_com')
-                    genuy, tokens = nltk_analyse.analyse(da, most_com)
-                    gemy = []
-                    gery = []
-                    for x,y in genuy:
-                        gemy.append([x,y])
-                    genuy.clear()
-                    for x in da:
-                        gery.append([x])
-                    da.clear()
-                    for token in tokens:
-                        all_tokens.append(token)
-                    if len(gery) >=1 and len(gemy) >=1:
-                        """ #put_code("*"*90)
-                        put_code(f'--{i} {user}: ')
-                        for j,ga in enumerate(da):
-                            put_code(f'[{j}] {ga}')
-                        e   lse:
-                        #print(da)
-                        continue
-                         da = popup(user, [
-                                put_html(f'<h3>{user} Сообщения:</h3>'),
-                                'html: <br/>',
-                                put_table([['Сообщения'], [da]]),
-                                put_button(['close_popup()'], onclick=lambda _x: close_popup())
-                            ])"""
-                        put_collapse(user,
-                        [
-                        f'Messages of {user}',
-                            put_table(gery
-                            , header=['Messages']),
-                            put_table(gemy
-                            , header=['word', 'count'])],
-                             open=False)
+        print(f"[{user}] error: {ex}")
 
 
-            except Exception as ex:
-                put_text(f"[{user}] error #8 {ex}")
-        except Exception as ex:
-            put_text(f'{ex} error #6')
-            continue
-    most_com = read_conf('most_com')
-    all_tokens,data = nltk_analyse.analyse_all(all_tokens,most_com)
-    all_chat = []
-    for i in all_tokens:
-        try:
-           all_chat.append([i[0],i[1]])
-        except Exception as ex:
-            put_text(f"error #7 {ex}")
-            pass
-    put_collapse(f'TOP words of {group_name}',[
-                    put_table(all_chat
-                    , header=['word']),
-                    ],open=False)
 
-    
-    put_button("Close",onclick=lambda: run_js('window.close()'), color='danger')
-    put_button("Scroll Up",onclick=lambda: run_js('window.scrollTo(document.body.scrollHeight, 0)'))
+# Общий анализ всех сообщений
+most_com = read_conf('most_com')
+all_tokens, data = nltk_analyse.analyse_all(all_tokens, most_com)
+all_chat = [[i[0], i[1]] for i in all_tokens]
+output.put_collapse(f'TOP words of {group_name}', [
+    output.put_table(all_chat, header=['word']),
+], open=False)
+
+all_sentiment_scores = [analyze_sentiment(msg) for msg in all_tokens]
+average_chat_sentiment = sum(all_sentiment_scores) / len(all_sentiment_scores) if all_sentiment_scores else 0
+output.put_text(f'Average Sentiment for {group_name}: {average_chat_sentiment:.2f}')
+
+
+# Обработка email и телефонов
+emaills = [[email] for email in set(emails)]
+phonness = [[ph] for ph in set(phoness)]
+output.put_collapse('Finded Emails and Numbers', [
+    output.put_table(emaills, header=['Emails:']),
+    output.put_table(phonness, header=['Numbers:'])
+], open=False)
+
+# Дополнительные кнопки
+output.put_button("Close", onclick=lambda: session.run_js('window.close()'), color='danger')
+output.put_button("Scroll Up", onclick=lambda: session.run_js('window.scrollTo(document.body.scrollHeight, 0)'))
